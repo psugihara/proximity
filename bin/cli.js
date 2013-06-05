12eee4a9ef4e1f9377cc6033a6a780c810ab7ab0
Andy Burke <aburke@bitflood.org>
2013-06-04 18:26:21 -0700

    Rework to be a bit simpler, but with more control.

--- bin/cli.js
+++ bin/cli.js
@@ -1,26 +1,79 @@
 #!/usr/bin/env node
 
+var watchTree = require('fs-watch-tree').watchTree;
+var exec = require('child_process').exec;
 var program = require('commander');
 
+var excludes = [];
+
 program
-    .usage('command path [paths...]');
+    .usage('[options] <command>')
+    .option('--exitonerror', 'Exit if the spawned command exits with an error code.')
+    .option('-x, --exclude <pattern>', 'Specify a string or regular expression which if matched, will ignore the change. You can specify multiple patterns to exclude by repeating this option.', function( val ) {
+        excludes.push( val );
+    })
+    .option('-t, --timeout', 'Specify a timeout (in milliseconds) for the spawned process. Default: none', 0)
+    .option('-v, --verbose', 'Output verbose information.')
+    .option('-q, --quiet', 'Supress command output.')
+    .parse(process.argv);
 
-program.on('--help', function(){
-    console.log('  Examples:');
-    console.log('');
-    console.log('    $ proximity "./build.sh" ./static/js');
-    console.log('');
-});
-
-program.parse(process.argv);
-
-if (program.args.length < 2) {
-    console.log('');
-    console.log('  Usage: proximity ' + program.usage());
-    console.log('');
-    process.exit(0);
+if (program.args.length != 1) {
+    program.help();
+    process.exit(1);
 }
 
-var command = program.args[0],
-    paths = program.args.slice(1),
-    proximity = require('proximity')(command, paths);
+var command = program.args[0];
+
+var ignored = null;
+if ( !!program.ignore )
+{
+    ignored = new RegExp( program.ignore );
+}
+
+function log(str) {
+    !!program.verbose && console.log( str );
+}
+
+log( 'exitonerror: ' + !!program.exitonerror );
+log( 'exclude: ' + excludes );
+log( 'quiet: ' + !!program.quiet );
+log( '' );
+
+var running = false;
+var lastChange = 0;
+watchTree('.', {
+    exclude: excludes
+}, function( event ) {
+    
+    if ( running )
+    {
+        log( '  ...command running, skipping change: ' + event.name );
+        return;
+    }
+    
+    running = true;
+
+    log( 'changed: ' + event.name );
+    log( '  spawning "' + command + '"' );
+    log( '' );
+
+    // Child will use parent's stdios
+    exec( command, {
+        timeout: program.timeout
+    }, function( error, stdout, stderr ) {
+        log( '' );
+        
+        !program.quiet && console.log( stdout );
+        !program.quiet && !!stderr && console.error( stderr );
+        !program.quiet && !!error && console.error( '  command exited with error code: ' + error.code );
+        
+        if ( program.exitonerror && !!error )
+        {
+            log( '  exiting' );
+            process.exit( error.code );
+        }
+
+        log( '  completed.' );
+        running = false;
+    });
+});
